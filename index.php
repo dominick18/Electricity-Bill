fe62f69d78f998374c6032958be030afd786c235
Dominick <dominick1@live.nl>
2023-10-16 14:29:08 +0200

    Updated index.php and added style.css + image

--- index.php
+++ index.php
@@ -19,22 +19,26 @@
         <link rel="stylesheet" href="style.css">
     </head>
     <body>
-        <header>
-            <h1>Electricity Bill Calculator</h1>
-        </header>
-        <div class="main-content">
-            <form action="data.php" method="POST">
-                <div class="form-group">
-                    <label for="usage">Usage:</label>
-                    <input type="text" name="usage">
-                </div>
-                <input type="submit" name="calculate">
-                <button name="reset">Reset</button>
-            </form>
-            <?php if(isset($errMsg)) {
-                echo '<h3>' . $errMsg . '</h3>';
-            } ?>
-            <h3>Your electricity bill is <?= $bill; ?></h3>
+        <div class="container">
+            <header class="flex">
+                <img src="lightning_icon.png" alt="lightning">
+                <h1>Electricity Bill Calculator</h1>
+                <img src="lightning_icon.png" alt="lightning">
+            </header>
+            <div class="main-content">
+                <form action="data.php" method="POST">
+                    <div class="form-group">
+                        <label for="usage">Usage:</label>
+                        <input type="text" name="usage">
+                    </div>
+                    <input type="submit" name="calculate" class="calculate">
+                    <button name="reset" class="reset">Reset</button>
+                </form>
+                <?php if(isset($errMsg)) {
+                    echo '<h3 class="error">' . $errMsg . '</h3>';
+                } ?>
+                <h3>Your electricity bill is <?= $bill; ?></h3>
+            </div>
         </div>
     </body>
 </html>
